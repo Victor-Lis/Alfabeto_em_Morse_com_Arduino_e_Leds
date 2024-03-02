eeb3a952dc6780a0c85ceb51584926d57701de04
Victor-Lis <victorlisbronzo1@gmail.com>
2024-03-02 16:43:22 -0300

    v1.02
    - Criando uma função para cada letra do alfabeto

--- led_codigo_morse.ino
+++ led_codigo_morse.ino
@@ -7,6 +7,7 @@ void setup()
 
 void loop()
 {
+  
 }
 
 void ponto(){
@@ -21,4 +22,190 @@ void traco(){
   delay(300); // Wait for 1000 millisecond(s)
   digitalWrite(led, LOW);
   delay(300); // Wait for 1000 millisecond(s)
+}
+
+void a(){
+  ponto();
+  traco();
+  delay(2000);
+}
+
+void b(){
+  traco();
+  ponto();
+  ponto();
+  ponto();
+  delay(2000);
+}
+
+void c(){
+  traco();
+  ponto();
+  traco();
+  ponto();
+  delay(2000);
+}
+
+void d(){
+  traco();
+  ponto();
+  ponto();
+  delay(2000);
+}
+
+void e(){
+  ponto();
+  delay(2000);
+}
+
+void f(){
+  ponto();
+  ponto();
+  traco();
+  ponto();
+  delay(2000);
+}
+
+void g(){
+  traco();
+  traco();
+  ponto();
+  delay(2000);
+}
+
+void h(){
+  ponto();
+  ponto();
+  ponto();
+  ponto();
+  delay(2000);
+}
+
+void i(){
+  ponto();
+  ponto();
+  delay(2000);
+}
+
+void j(){
+  ponto();
+  traco();
+  traco();
+  traco();
+  delay(2000);
+}
+
+void k(){
+  ponto();
+  traco();
+  ponto();
+  delay(2000);
+}
+
+void l(){
+  ponto();
+  traco();
+  ponto();
+  ponto();
+  delay(2000);
+}
+
+void m(){
+  traco();
+  traco();
+  delay(2000);
+}
+
+void n(){
+  traco();
+  ponto();
+  delay(2000);
+}
+
+void o(){
+  traco();
+  traco();
+  traco();
+  delay(2000);
+}
+
+void p(){
+  ponto();
+  traco();
+  traco();
+  ponto();
+  delay(2000);
+}
+
+void q(){
+  traco();
+  traco();
+  ponto();
+  traco();
+  delay(2000);
+}
+
+void r(){
+  ponto();
+  traco();
+  ponto();
+  delay(2000);
+}
+
+void s(){
+  ponto();
+  ponto();
+  ponto();
+  delay(2000);
+}
+
+void t(){
+  traco();
+  delay(2000);
+}
+
+void u(){
+  ponto();
+  ponto();
+  traco();
+  delay(2000);
+}
+
+void v(){
+  ponto();
+  ponto();
+  ponto();
+  traco();
+  delay(2000);
+}
+
+void w(){
+  ponto();
+  traco();
+  traco();
+  delay(2000);
+}
+
+void x(){
+  traco();
+  ponto();
+  ponto();
+  traco();
+  delay(2000);
+}
+
+void y(){
+  traco();
+  ponto();
+  traco();
+  traco();
+  delay(2000);
+}
+
+void z(){
+  traco();
+  traco();
+  ponto();
+  ponto();
+  delay(2000);
 }
