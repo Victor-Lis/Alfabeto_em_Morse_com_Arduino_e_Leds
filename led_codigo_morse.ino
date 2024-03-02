2609f84265703c41391c0ff293b058187611a001
Victor-Lis <victorlisbronzo1@gmail.com>
2024-03-02 16:31:55 -0300

    v1.01
    - Adicionando funções "ponto" e "traco", para facilitar o código morse

--- led_codigo_morse.ino
+++ led_codigo_morse.ino
@@ -7,5 +7,18 @@ void setup()
 
 void loop()
 {
-  
+}
+
+void ponto(){
+  digitalWrite(led, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(led, LOW);
+  delay(300); // Wait for 1000 millisecond(s)
+}
+
+void traco(){
+  digitalWrite(led, HIGH);
+  delay(300); // Wait for 1000 millisecond(s)
+  digitalWrite(led, LOW);
+  delay(300); // Wait for 1000 millisecond(s)
 }
